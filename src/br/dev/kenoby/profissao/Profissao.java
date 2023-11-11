b635c69b5f5c4cd96f269c0b32de0a226c0552bf
carlosjr <josecarlosoares7@gmail.com>
2023-11-10 23:12:34 -0300

    Criando a Classe Profissão

--- src/br/dev/kenoby/profissao/Profissao.java
+++ src/br/dev/kenoby/profissao/Profissao.java
@@ -1,4 +1,17 @@
 package br.dev.kenoby.profissao;
 
 public class Profissao {
+    private String nomeProfissao;
+
+    public void cadastrarProfissao(String nomeProfissao){
+        this.nomeProfissao = nomeProfissao;
+    }
+
+    public String getNomeProfissao() {
+        return nomeProfissao;
+    }
+
+    public void setNomeProfissao(String nomeProfissao) {
+        this.nomeProfissao = nomeProfissao;
+    }
 }
