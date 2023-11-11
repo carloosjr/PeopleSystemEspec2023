d5e78d8e661183da0caf7b6db6b1416289d1a8e3
carlosjr <josecarlosoares7@gmail.com>
2023-11-11 00:36:18 -0300

    Criando os objetos

--- src/br/dev/kenoby/appMain/AppMain.java
+++ src/br/dev/kenoby/appMain/AppMain.java
@@ -1,12 +1,26 @@
 package br.dev.kenoby.appMain;
 
 //Importando tudo do pacote pessoas
+import br.dev.kenoby.endereco.Endereco;
 import br.dev.kenoby.pessoas.*;
+import br.dev.kenoby.profissao.Profissao;
+import br.dev.kenoby.telefone.Telefone;
+
+import java.util.Scanner;
 
 //Onde será chamada a aplicação
 public class AppMain {
     public static void main(String[] args) {
-//Instanciando os objetos
+        //Instanciando os objetos
+        Cliente objetoCliente = new Cliente();
+        Endereco objetoEndereco = new Endereco();
+        Profissao objetoProfissao = new Profissao();
+        Telefone objetoTelfone = new Telefone();
+
+        objetoCliente.cadastrar("Carlos Jr", "16/02/1995");
+        objetoEndereco.cadastrarEndereco("Rua das puras", "250", "580000", "Joao pessoa", "PB", "BR");
+        objetoProfissao.cadastrarProfissao("Analista de TI");
+        objetoTelfone.cadastrarTelefone("55", "83", "999999999", "celular");
 
     }
 }
